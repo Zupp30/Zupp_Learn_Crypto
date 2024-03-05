829a418aab90323ba6bcf459ed34190c0dee1c31
Zupp30 <ndhd3011@gmail.com>
2024-03-05 23:17:50 +0700

    Delete file

--- CTF Archive/2020/Sign in Please (HKCERT CTF)/Solve.py
+++
@@ -1,9 +0,0 @@
-# archive.cryptohack.org 1024
-from pwn import *
-from json import *
-
-def send_request(hsh):
-    return s.sendline(dumps(hsh).encode())
-
-s = connect("archive.cryptohack.org", 1024)
-print(s.recv())
